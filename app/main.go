6af086c03675ecfd6f1d338b08af675ab115031c
takusan <takusan9170@gmail.com>
2025-03-16 16:20:40 +0900

    最新バージョンにしたら全てうまくいったので最終テスト

--- app/main.go
+++ app/main.go
@@ -11,6 +11,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, world!")
 	fmt.Fprintln(w, "test push next to next")
+	fmt.Fprintln(w, "final test")
 	// 環境変数次第で Data() と Data2() のどちらかを呼び出す
 	var data []string
 	if os.Getenv("USE_LOCAL_DB") == "true" {
